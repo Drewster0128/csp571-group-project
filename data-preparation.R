0e54eb67c3e25a69a7fc1c4e492f667388a3ef7d
Drewster0128 <acook21@hawk.iit.edu>
2025-04-18 08:39:46 -0500

    More feature classification

--- data-preparation.R
+++ data-preparation.R
@@ -6,8 +6,8 @@ PCOS_data <- read_xlsx("PCOS_data/PCOS_data_without_infertility.xlsx",
 
 #split into demographic, clinical, and lifestyle attributes
 demographic_features <- c("Age (yrs)", "Weight (Kg)", "Height(Cm)", "BMI", "Blood Group", "Hip(inch)", "Waist(inch)", "Waist:Hip Ratio", "Pimples(Y/N)", "Endometrium (mm)")
-clinical_features <- c("Pulse rate(bpm)", "RR (breaths/min)", "Hb(g/dl)", "Cycle(R/I)", "Cycle length(days)", "RBS(mg/dl)", "BP _Systolic (mmHg)", "BP _Diastolic (mmHg)", "Follicle No. (L)", "Follicle No. (R)", "Avg. F Size (L) (mm)", "Avg. F Size (R) (mm)", "TSH (mIU/L)", "AMH(ng/mL)", "PRL(ng/mL)", "Vit D3 (ng/mL)","I   beta-HCG(mIU/mL)", "FSH(mIU/mL)", "LH(mIU/mL)", "FSH/LH")
-lifestyle_features <- c("Marriage Status (Yrs)", "Fast food (Y/N)", "Reg.Exercise(Y/N)", "Pregant(Y/N)", "No. of aborptions")
+clinical_features <- c("Pulse rate(bpm)", "RR (breaths/min)", "Hb(g/dl)", "Cycle(R/I)", "Cycle length(days)", "RBS(mg/dl)", "BP _Systolic (mmHg)", "BP _Diastolic (mmHg)", "Follicle No. (L)", "Follicle No. (R)", "Avg. F size (L) (mm)", "Avg. F size (R) (mm)", "TSH (mIU/L)", "AMH(ng/mL)", "PRL(ng/mL)", "Vit D3 (ng/mL)","I   beta-HCG(mIU/mL)", "FSH(mIU/mL)", "LH(mIU/mL)", "FSH/LH", "PRG(ng/mL)")
+lifestyle_features <- c("Marraige Status (Yrs)", "Fast food (Y/N)", "Reg.Exercise(Y/N)", "Pregnant(Y/N)", "No. of aborptions")
 
 #drop some unnecessary features
 uncessary_features <- c("Sl. No", "Patient File No.", "II    beta-HCG(mIU/mL)", "...45")
