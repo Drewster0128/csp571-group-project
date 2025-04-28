25aae101ab362b8e99688645054c44223d08e323
Drewster0128 <acook21@hawk.iit.edu>
2025-04-28 15:10:08 -0500

    Clinical feature visualizations

--- data-exploration.R
+++ data-exploration.R
@@ -36,3 +36,38 @@ for(feature in demographic_features)
          main = sprintf("Histogram of %s", feature))
   }
 }
+
+#data visualization with clinical data
+clinical_features <- c("Pulse rate(bpm)", "RR (breaths/min)", "Hb(g/dl)", "Cycle(R/I)", "Cycle length(days)", "RBS(mg/dl)", "BP _Systolic (mmHg)", "BP _Diastolic (mmHg)", "Follicle No. (L)", "Follicle No. (R)", "Avg. F size (L) (mm)", "Avg. F size (R) (mm)", "TSH (mIU/L)", "AMH(ng/mL)", "PRL(ng/mL)", "Vit D3 (ng/mL)", "I   beta-HCG(mIU/mL)", "FSH(mIU/mL)", "LH(mIU/mL)", "FSH/LH", "PRG(ng/mL)", "`Weight gain(Y/N)`1", "`hair growth(Y/N)`1", "`Skin darkening (Y/N)`1", "`Hair loss(Y/N)`1")
+clinical_data <- PCOS_data[, clinical_features]
+
+for(feature in clinical_features)
+{
+  if(feature %in% categorical_features)
+  {
+    yes_has_pcos_freq <- nrow(PCOS_data[(PCOS_data$`PCOS (Y/N)` == 1 & PCOS_data[, feature] == 1),])
+    no_has_pcos_freq <- nrow(PCOS_data[(PCOS_data$`PCOS (Y/N)` == 1 & PCOS_data[, feature] == 0),])
+    
+    yes_no_pcos_freq <- nrow(PCOS_data[(PCOS_data$`PCOS (Y/N)` == 0 & PCOS_data[, feature] == 1),])
+    no_no_pcos_freq <- nrow(PCOS_data[(PCOS_data$`PCOS (Y/N)` == 0 & PCOS_data[, feature] == 0),])
+    
+    values <- matrix(c(yes_has_pcos_freq, no_has_pcos_freq, yes_no_pcos_freq, no_no_pcos_freq), ncol = 2, byrow = TRUE)
+    
+    barplot(values, col = c("red", "blue"), names.arg = c("Yes", "No"), xlab = feature, ylab = "Frequency", main = sprintf("Distribution of observations with %s", feature))
+    legend('topleft', c("Has PCOS", "Doesn't have PCOS"), fill=c("red", "blue"))
+  }
+  else
+  {
+    plot(x = as.factor(Y),
+         y = PCOS_data[, feature],
+         xlab = "PCOS",
+         ylab = feature,
+         col = c("blue", "red"),
+         main = sprintf("Box Plot of %s", feature))
+    hist(PCOS_data[, feature],
+         xlab = feature,
+         ylab = "Frequency",
+         col = "blue",
+         main = sprintf("Histogram of %s", feature))
+  }
+}
